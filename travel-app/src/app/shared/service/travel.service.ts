3fe088752952a227de3eed862dc72baa907b39ab
redu <reduardo19942@gmail.com>
2021-02-07 14:17:45 -0300

    post front end feito

--- travel-app/src/app/shared/service/travel.service.ts
+++ travel-app/src/app/shared/service/travel.service.ts
@@ -2,12 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { responsePageable } from '../model/responsePageable.model';
+import { DestinationCity } from '../model/destination-city.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TravelService {
-
   apiUrl = "http://localhost:8080/api/travel";
   httpOptions = {
     headers: new HttpHeaders({
@@ -19,5 +19,9 @@ export class TravelService {
 
   public getAll(): Observable<responsePageable> {
     return this.httpClient.get<responsePageable>(this.apiUrl);
+  }
+
+  public postTravel(travel: any): Observable<DestinationCity> {
+    return this.httpClient.post<any>(this.apiUrl, travel, this.httpOptions);
   }
 }
--- travel-app/src/app/views/home/new-travel/new-travel.component.ts
+++ travel-app/src/app/views/home/new-travel/new-travel.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { TravelService } from 'src/app/shared/service/travel.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-new-travel',
@@ -32,5 +33,14 @@ export class NewTravelComponent implements OnInit {
   close(){
     this.dialogRef.close(true);
     this.travelForm.reset();
+  }
+
+  scheduleTrip(){
+    let travelDate: moment.Moment = moment.utc(this.travelForm.value.date).local();
+    this.travelForm.value.date = travelDate.format("YYYY-MM-DD") + 'T' + this.travelForm.value.hora;
+    this.rest.postTravel(this.travelForm.value).subscribe(result => {});
+    this.dialogRef.close(true);
+    this.travelForm.reset();
+    window.location.reload();
   }
 }
